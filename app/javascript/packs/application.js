e87fd1de0851cb150efbfaef2c2fc04ca49d2df8
Blake.Grabsky <Blake.Grabsky@medecision.com>
2019-12-09 10:01:52 -0600

    Add favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,10 @@ require("channels")
 require('jquery')
 require('semantic-ui')
 
+$(document).on('turbolinks:load', function() {
+	$('.ui.dropdown').dropdown();
+})
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
