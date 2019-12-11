ecdff0a3bcdb4298cc24f9d24e37c7bddbd97d42
Blake.Grabsky <Blake.Grabsky@medecision.com>
2019-12-11 11:51:21 -0600

    Beautify input box, functionality, and create custom scope

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,10 @@
 class Message < ApplicationRecord
 	belongs_to :user
+	
 	validates :body, 
 	presence:true,
 	length: {minimum: 1, maximum: 150 }
+	
+	#return the last 20 created messages
+	scope :custom_display, -> { order(:created_at).last(14) }
 end
